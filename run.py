b80c4a4f580832cfcc7434479c7a9677dcff7648
Ann Edvinsson <aslin.edvinsson@gmail.com>
2023-11-16 08:29:02 +0000

    What I said befor

--- run.py
+++ run.py
@@ -108,14 +108,12 @@ def get_last_5_entries_sales():
     the last 5 entries for each sandwich and returns the data as a list of lists.
     """
     sales = SHEET.worksheet('sales')
-    #column = sales.col_values(3)
-    #print(column)
 
     columns = []
     for ind in range(1,7):
         column = sales.col_values(ind)
-        columns.append(column)
-    pprint(columns)
+        columns.append([-5:])
+    return columns
 
 def main():
     """
@@ -131,4 +129,4 @@ def main():
 print('Welcome to Love Sandwiches Data Automation')
 #main()
 
-get_last_5_entries_sales()
+sales_columns = get_last_5_entries_sales()
