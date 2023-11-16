96d6482ff95e19321420b7a30bbf21b00f6255bb
Ann Edvinsson <aslin.edvinsson@gmail.com>
2023-11-16 10:35:53 +0000

    Calculate stock numbers and add to stock worksheet

--- run.py
+++ run.py
@@ -112,8 +112,24 @@ def get_last_5_entries_sales():
     columns = []
     for ind in range(1,7):
         column = sales.col_values(ind)
-        columns.append([-5:])
+        columns.append(column[-5:])
+
     return columns
+
+def calculate_stock_data(data):
+    """
+    Calculate the avarage stock for each item tyoe, adding 10%
+    """
+    print('Calculationg stock data...\n')
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        avarage = sum(int_column) / len(int_column)
+        stock_num = avarage * 1.1
+        new_stock_data.append(round(stock_num))
+    
+    return new_stock_data
 
 def main():
     """
@@ -124,9 +140,10 @@ def main():
     update_worksheet(sales_data, 'sales')
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, 'surplus')
-    
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, 'stock')
 
 print('Welcome to Love Sandwiches Data Automation')
-#main()
+main()
 
-sales_columns = get_last_5_entries_sales()
